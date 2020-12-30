4f0cbd4f1e9f7a4c52125962462a2b2ef9d6e826
Bla <zog.gen@hotmail.de>
2020-12-30 23:36:48 +0100

    First

---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double Pferd = 150;
+            double Stein = 3;
+            double Stroh = 4;
+            ArrayList Items = new ArrayList();
+            Items.Add(Pferd);
+            Items.Add(Stein);
+            Items.Add(Stroh);
+
+            void priceChange(ArrayList T)
+            {
+                foreach (double item in Items)
+                {
+                    Console.WriteLine(item);
+                }
+
+
+           while(true)
+            { 
+
+            foreach (double item in Items) {
+                Console.WriteLine(item);
+            }
+
+            Thread.Sleep(2000);
+            }
+            Console.ReadLine();
+
+
+        }
+    }
+}
