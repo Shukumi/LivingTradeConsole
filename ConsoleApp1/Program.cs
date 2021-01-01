87b4e90c344da5aa4f66134056803e101dbcc24f
Shu <jmxszg@gmail.com>
2021-01-02 00:25:18 +0100

    Implemented 3 Trends, working

--- ConsoleApp1/Program.cs
+++ ConsoleApp1/Program.cs
@@ -12,13 +12,19 @@ namespace ConsoleApp1
     {
         static void Main(string[] args)
         {
+            double GetRandomNumber(double minimum, double maximum)
+            {
+                Random r = new Random();
+                return r.NextDouble() * (maximum - minimum) + minimum;
+            }
+
             var rand = new Random();
             double Pferd = 150;
             double Stein = 3;
             double Stroh = 4;
             int Tag = 0;
             int NextTrendStart = 0;
-            string AktuTrend = ""
+            string AktuTrend = "";
             ArrayList Items = new ArrayList();
             Items.Add(Pferd);
             Items.Add(Stein);
@@ -28,29 +34,81 @@ namespace ConsoleApp1
             void upTrend(ArrayList T)
             {
                 Console.WriteLine(10);
-                NextTrendStart = Tag + 7;
-
+                AktuTrend = "up";
 
             }
 
             void downTrend(ArrayList T)
             {
                 Console.WriteLine(40);
-                NextTrendStart = Tag + 7;
+                AktuTrend = "down";
 
             }
 
             void riskyTrend(ArrayList T)
             {
                 Console.WriteLine(60);
-                NextTrendStart = Tag + 7;
+                AktuTrend = "risky";
 
             }
 
+            ArrayList priceChange(ArrayList T)
+            {
+                ArrayList Z = new ArrayList();
+                switch (AktuTrend)
+                {
+                    case "up":
+                        
+                        {
+                            double r = GetRandomNumber(1.00,1.1);
+                            foreach (double Item in T)
+                            { 
+                                double newItem = Item * r;
+                                newItem = Math.Round(newItem, 2);
+                                Z.Add(newItem);
+                                Console.WriteLine(Item);
+                                Console.WriteLine(newItem);
+                            }
+                            break;
+                        }
+                        
+                    case "down": 
+                        {
+                            double r = GetRandomNumber(0.90, 1.00);
+                            foreach (double Item in T)
+                            {
+                                double newItem = Item * r;
+                                newItem = Math.Round(newItem, 2);
+                                Z.Add(newItem);
+                                Console.WriteLine(Item);
+                                Console.WriteLine(newItem);
+                            }
+                            break;
+                        }
+                        
+                    case "risky":
+                        {
+                            double r = GetRandomNumber(0.75, 1.25);
+                            int t = rand.Next(2);
 
+                            foreach (double Item in T)
+                            {
+                                double newItem = Item * r;
+                                newItem = Math.Round(newItem, 2);
+                                Z.Add(newItem);
+                                Console.WriteLine(Item);
+                                Console.WriteLine(newItem);
+                            }
+                            break;
+                        }
+                        
+                }
+                return Z;
+            }
 
+            
 
-            void priceChange(ArrayList T)
+            void definePriceChange()
             {
                 int r = rand.Next(101);
                 Console.WriteLine("Hey" + r);
@@ -73,16 +131,27 @@ namespace ConsoleApp1
 
            while(true)
             {
-                Tag += 1;
+                
                 if (Tag == NextTrendStart)
                 { 
-                  priceChange(Items);
+                    definePriceChange();
+                    NextTrendStart = Tag + 7;
                 }
-                //foreach (double item in Items) {
-                //    Console.WriteLine(item);
-                //}
-                
-                Thread.Sleep(5000);
+                Tag += 1;
+                Console.WriteLine("Tag: " + Tag);
+
+
+
+                Items = priceChange(Items);
+
+                int n = 0;
+                foreach (double item in Items)
+                {
+                    n += 1;
+                    Console.WriteLine("Item " + n + ": " + item);
+                }
+
+                Thread.Sleep(8000);
             }
             Console.ReadLine();
 
